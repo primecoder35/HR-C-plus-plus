aab4444d1e8b5b721dd1d373229d82183b551c47
primecoder35 <jawesome93@gmail.com>
2017-10-07 22:23:42 -0400

    Update functions.cpp

--- Introduction/functions.cpp
+++ Introduction/functions.cpp
@@ -2,9 +2,8 @@
 #include <cstdio>
 using namespace std;
 
-/*
-Add `int max_of_four(int a, int b, int c, int d)` here.
-*/
+/* Find the maximum of the four inputs */
+
 int max_of_four(int a, int b, int c, int d)
 {
     int max;
