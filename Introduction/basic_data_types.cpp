fab277b9ecb8cc1465d99423ba8c2d464f37136a
primecoder35 <jawesome93@gmail.com>
2017-10-07 21:59:56 -0400

    Create basic_data_types.cpp

---
+++ Introduction/basic_data_types.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+    
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    cin >> d;
+    cin >> e;
+    printf("%d\n%li\n%c\n%f\n%f", a, b, c, d, e);
+    
+
+    return 0;
+}
