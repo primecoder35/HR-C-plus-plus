c6bcc46e3b0c557eab597519017a4c30a3e928a5
primecoder35 <jawesome93@gmail.com>
2017-10-07 22:08:37 -0400

    Create input_output.cpp

---
+++ Introduction/input_output.cpp
@@ -0,0 +1,19 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    int a,b,c;
+    int tot;
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    tot = a+b+c;
+    cout << tot;
+    return 0;
+}
